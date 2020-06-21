4df6c41dd69aa9bb8bdd16bdea6ff237900ffc0c
Trieu Nguyen <47194907+HaiTrieuNg@users.noreply.github.com>
2020-06-21 02:05:25 -0700

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -62,10 +62,12 @@ master_doc = 'index'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
+
 html_static_path = ['_static']
 
-
-html_css_files = [
-    'css/theme - Copy.css',
-]
+html_context = {
+    'css_files': [
+        '_static/theme - Copy.css.css',  # override wide tables in RTD theme
+        ],
+     }
 
