2dafcef02646e17d71f5ed316672397cbc5e14c1
Trieu Nguyen <47194907+HaiTrieuNg@users.noreply.github.com>
2020-06-21 01:29:50 -0700

    Add files via upload

--- source/conf.py
+++ source/conf.py
@@ -63,10 +63,9 @@ master_doc = 'index'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-def setup(app):
-    app.add_stylesheet("theme - Copy.css")
 
-#html_css_files = [
-#    'css/theme - Copy.css',
-#]
+
+html_css_files = [
+    'css/theme - Copy.css',
+]
 
